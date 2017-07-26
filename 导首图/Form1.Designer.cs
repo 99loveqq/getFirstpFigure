eff03ce3657459011152b3a8ec1003c016a6f052
王应泽 <496954276@qq.com>
2017-07-26 16:31:41 +0800

    first

---
+++ 导首图/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace 导首图
+{
+    partial class Form1
+    {
+            /// <summary>
+            /// 必需的设计器变量。
+            /// </summary>
+            private System.ComponentModel.IContainer components = null;
+
+            /// <summary>
+            /// 清理所有正在使用的资源。
+            /// </summary>
+            /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+            #region Windows 窗体设计器生成的代码
+
+            /// <summary>
+            /// 设计器支持所需的方法 - 不要
+            /// 使用代码编辑器修改此方法的内容。
+            /// </summary>
+            private void InitializeComponent()
+            {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHuoQu = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.liv = new System.Windows.Forms.ListView();
+            this.标题 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.网址 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.txtLuJing = new System.Windows.Forms.TextBox();
+            this.txtWjm = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.pbr = new System.Windows.Forms.ProgressBar();
+            this.btnLiuL = new System.Windows.Forms.Button();
+            this.btnDownLoad = new System.Windows.Forms.Button();
+            this.btnQingKong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(131, 12);
+            this.txtUrl.MaxLength = 1000000;
+            this.txtUrl.Multiline = true;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(598, 76);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请输入获取图片信息";
+            // 
+            // btnHuoQu
+            // 
+            this.btnHuoQu.Location = new System.Drawing.Point(755, 29);
+            this.btnHuoQu.Name = "btnHuoQu";
+            this.btnHuoQu.Size = new System.Drawing.Size(75, 23);
+            this.btnHuoQu.TabIndex = 2;
+            this.btnHuoQu.Text = "获  取";
+            this.btnHuoQu.UseVisualStyleBackColor = true;
+            this.btnHuoQu.Click += new System.EventHandler(this.btnHuoQu_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(735, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "111";
+            // 
+            // liv
+            // 
+            this.liv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.标题,
+            this.网址});
+            this.liv.GridLines = true;
+            this.liv.Location = new System.Drawing.Point(14, 100);
+            this.liv.Name = "liv";
+            this.liv.Size = new System.Drawing.Size(858, 268);
+            this.liv.TabIndex = 4;
+            this.liv.UseCompatibleStateImageBehavior = false;
+            this.liv.View = System.Windows.Forms.View.Details;
+            // 
+            // 标题
+            // 
+            this.标题.Tag = "";
+            this.标题.Text = "标题";
+            this.标题.Width = 412;
+            // 
+            // 网址
+            // 
+            this.网址.Text = "网址";
+            this.网址.Width = 436;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "路径";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(304, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "文件夹名";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "进度条";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 438);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(836, 438);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(11, 12);
+            this.lblMax.TabIndex = 9;
+            this.lblMax.Text = "0";
+            // 
+            // txtLuJing
+            // 
+            this.txtLuJing.Location = new System.Drawing.Point(47, 391);
+            this.txtLuJing.Name = "txtLuJing";
+            this.txtLuJing.ReadOnly = true;
+            this.txtLuJing.Size = new System.Drawing.Size(251, 21);
+            this.txtLuJing.TabIndex = 10;
+            // 
+            // txtWjm
+            // 
+            this.txtWjm.Location = new System.Drawing.Point(363, 391);
+            this.txtWjm.Name = "txtWjm";
+            this.txtWjm.Size = new System.Drawing.Size(245, 21);
+            this.txtWjm.TabIndex = 11;
+            // 
+            // pbr
+            // 
+            this.pbr.Location = new System.Drawing.Point(74, 431);
+            this.pbr.Name = "pbr";
+            this.pbr.Size = new System.Drawing.Size(756, 23);
+            this.pbr.TabIndex = 12;
+            // 
+            // btnLiuL
+            // 
+            this.btnLiuL.Location = new System.Drawing.Point(614, 389);
+            this.btnLiuL.Name = "btnLiuL";
+            this.btnLiuL.Size = new System.Drawing.Size(75, 23);
+            this.btnLiuL.TabIndex = 13;
+            this.btnLiuL.Text = "浏览";
+            this.btnLiuL.UseVisualStyleBackColor = true;
+            this.btnLiuL.Click += new System.EventHandler(this.btnLiuL_Click);
+            // 
+            // btnDownLoad
+            // 
+            this.btnDownLoad.Location = new System.Drawing.Point(699, 389);
+            this.btnDownLoad.Name = "btnDownLoad";
+            this.btnDownLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnDownLoad.TabIndex = 14;
+            this.btnDownLoad.Text = "下载";
+            this.btnDownLoad.UseVisualStyleBackColor = true;
+            this.btnDownLoad.Click += new System.EventHandler(this.btnDownLoad_Click);
+            // 
+            // btnQingKong
+            // 
+            this.btnQingKong.Location = new System.Drawing.Point(787, 389);
+            this.btnQingKong.Name = "btnQingKong";
+            this.btnQingKong.Size = new System.Drawing.Size(75, 23);
+            this.btnQingKong.TabIndex = 15;
+            this.btnQingKong.Text = "清空";
+            this.btnQingKong.UseVisualStyleBackColor = true;
+            this.btnQingKong.Click += new System.EventHandler(this.btnQingKong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 477);
+            this.Controls.Add(this.btnQingKong);
+            this.Controls.Add(this.btnDownLoad);
+            this.Controls.Add(this.btnLiuL);
+            this.Controls.Add(this.pbr);
+            this.Controls.Add(this.txtWjm);
+            this.Controls.Add(this.txtLuJing);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.liv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnHuoQu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "导首图2.05.24";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+            #endregion
+
+            private System.Windows.Forms.TextBox txtUrl;
+            private System.Windows.Forms.Label label1;
+            private System.Windows.Forms.Button btnHuoQu;
+            private System.Windows.Forms.Label label3;
+            private System.Windows.Forms.ListView liv;
+            private System.Windows.Forms.ColumnHeader 标题;
+            private System.Windows.Forms.ColumnHeader 网址;
+            private System.Windows.Forms.Label label2;
+            private System.Windows.Forms.Label label4;
+            private System.Windows.Forms.Label label5;
+            private System.Windows.Forms.Label label6;
+            private System.Windows.Forms.Label lblMax;
+            private System.Windows.Forms.TextBox txtLuJing;
+            private System.Windows.Forms.TextBox txtWjm;
+            private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+            private System.Windows.Forms.ProgressBar pbr;
+            private System.Windows.Forms.Button btnLiuL;
+            private System.Windows.Forms.Button btnDownLoad;
+            private System.Windows.Forms.Button btnQingKong;
+    }
+}
+
---
+++ 导首图/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace 导首图
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //获取指定网页的HTML代码
+        static string GetPageSource(string URL)
+        {
+            WebClient wc = new WebClient();
+            wc.Credentials = CredentialCache.DefaultCredentials;
+
+            Encoding enc = Encoding.GetEncoding("GBK");
+            Byte[] pageData = wc.DownloadData(URL);
+            string x = enc.GetString(pageData);
+            return x;
+        }
+        //获取图片对应的字符串
+        static ArrayList GetH(string htmlCode)
+        {
+            ArrayList al = new ArrayList();
+            string strRegex = string.Format(@"target=""_blank""><img src=""https://[\w\W]*?<i title=""修改标题""");
+            Regex r = new Regex(strRegex, RegexOptions.IgnoreCase);
+            MatchCollection m = r.Matches(htmlCode);
+            for (int i = 0; i < m.Count; i++)
+            {
+                bool rep = false;
+                string strNew = m[i].ToString();
+                // 过滤重复的URL
+                foreach (string str in al)
+                {
+                    if (strNew == str)
+                    {
+                        rep = true;
+                        break;
+                    }
+                }
+                if (!rep) al.Add(strNew);
+            }
+            al.Sort();
+            return al;
+        }
+        string path = "E:";//文件路径  
+
+        //清空、初始
+        private void btnQingKong_Click(object sender, EventArgs e)
+        {
+            liv.Items.Clear();
+            label3.Text = string.Format("总共有{0}张图片可下载", this.liv.Items.Count.ToString());
+            pbr.Value = 0;
+            lblMax.Text = "0";
+        }
+
+        //下载图片
+        private void btnDownLoad_Click(object sender, EventArgs e)
+        {
+            //pbr=new ProgressBar();
+            pbr.Maximum = this.liv.Items.Count;
+            if (txtLuJing.Text.Trim() == "")
+            { MessageBox.Show("请选择路劲", "提示"); }
+            else
+            {
+                if (txtWjm.Text.Trim() == "")
+                {
+                    MessageBox.Show("请输入文件夹名", "提示");
+                    txtWjm.Text = "";
+                    txtWjm.Focus();
+                }
+                else
+                {
+                    int downnum = 0;
+                    string path = string.Format(this.path + "\\" + txtWjm.Text + "\\");
+                    System.IO.Directory.CreateDirectory(path);
+                    WebClient client = new WebClient();
+                    string except_chars = ": \\ / * ? \" > < |";
+                    int a = 0;
+                    for (int i = 0; i < liv.Items.Count; i++)
+                    {
+                        string strName = this.liv.Items[i].Text.ToString();
+                        strName = Regex.Replace(strName, "[" + Regex.Escape(except_chars) + "]", " ");
+                        string strUrl = this.liv.Items[i].SubItems[1].Text.ToString();
+                        strUrl = strUrl.Replace(".100x100xz.", ".");
+                        try
+                        {
+                            client.DownloadFile(strUrl, path + System.IO.Path.GetFileName(strName + ".jpg"));
+                            downnum++;
+                            a = 0;
+                        }
+                        catch (Exception)
+                        {
+                            i--;
+                            a++;
+                            if (a == 3)
+                            {
+                                i++;
+                            }
+                        }
+                        pbr.Value = i + 1;
+                    }
+                    string num = string.Format("成功下载了{0}张首图", downnum);
+                    MessageBox.Show(num, "提示");
+                }
+            }
+        }
+
+        //获取用户选中路径
+        private void btnLiuL_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = this.folderBrowserDialog.SelectedPath;
+                txtLuJing.Text = path;
+            }
+        }
+
+        //获取代码链接
+        private void btnHuoQu_Click(object sender, EventArgs e)
+        {
+            if (txtUrl.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入代码", "提示");
+                txtUrl.Focus();
+            }
+            else
+            {
+                string x = txtUrl.Text;
+                ArrayList url = GetH(x);
+
+                for (int i = 0; i < url.Count; i++)
+                {
+                    string str = url[i].ToString();
+                    int a = str.IndexOf("img src=\"")+9;
+                    int b = str.IndexOf("\" alt=\"\">");
+                    string strUrl = str.Substring(a, b - a);
+                    int d = str.IndexOf("\" target=\"_blank\">")+18;
+                    string f = str.Substring(d , str.Length - d-19);
+                    string strName = string.Format(f);
+                    ListViewItem liv = new ListViewItem(strName);
+                    liv.SubItems.Add(strUrl);
+                    this.liv.Items.Add(liv);
+                    //过滤相同图片链接
+                    for (int k = 1; k < this.liv.Items.Count - 1; k++)
+                    {
+                        for (int j = k + 1; j < this.liv.Items.Count; j++)
+                        {
+                            if (this.liv.Items[k].SubItems[1].Text.Trim() == this.liv.Items[j].SubItems[1].Text.Trim())
+                            {
+                                this.liv.Items.Remove(this.liv.Items[j]);
+                            }
+                        }
+                    }
+                    for (int k = 1; k < this.liv.Items.Count - 1; k++)
+                    {
+                        for (int j = k + 1; j < this.liv.Items.Count; j++)
+                        {
+                            if (this.liv.Items[k].SubItems[0].Text.Trim() == this.liv.Items[j].SubItems[0].Text.Trim())
+                            {
+                                this.liv.Items[j].SubItems[0].Text = this.liv.Items[j].SubItems[0].Text + "-1";
+                            }
+                        }
+                    }
+                }
+            }
+            label3.Text = string.Format("总共有{0}张图片可下载", this.liv.Items.Count.ToString());
+            txtUrl.Text = "";
+            txtUrl.Focus();
+            lblMax.Text = this.liv.Items.Count.ToString();
+        }
+
+        //窗体加载
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Text = "总共有0张图片可下载";
+            DateTime DT = System.DateTime.Now;
+            string dt = System.DateTime.Now.ToShortDateString();
+            dt = dt.Substring(5, dt.Length - 5);
+            dt = dt.Replace("/", ".");
+            txtWjm.Text = string.Format("{0}【整单首图】公司名-姓名", dt);
+        }
+    }
+}
